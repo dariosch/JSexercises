0c664201f30a82bec91ce08686e637163d3399ca
Dario <dario.sch@gmail.com>
2022-11-21 12:13:35 -0300

    Funcionalidad del juego finalizada

--- 7/codingchallenge4.js
+++ 7/codingchallenge4.js
@@ -12,27 +12,59 @@ Tasks:
 4- Also restore the original background color (#222) and number width (15rem)
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
-document.querySelector(".number").textContent = secretNumber;
+let highScore = 0;
 
 document.querySelector(".score").textContent = score;
-document.querySelector(".guess").addEventListener("click", function () {
+document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   if (!guess) {
     document.querySelector(".message").textContent = "Ingresa un numero";
   } else if (guess === secretNumber) {
-    document.querySelector(".message").textContent = "Adivinaste!";
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector(".message").textContent = "Adivinaste! 🙌";
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
   } else if (guess > secretNumber) {
-    document.querySelector(".message").textContent = "Muy alto!";
-    score--;
-    document.querySelector(".score").textContent = score;
+    if (score == 1) {
+      document.querySelector(".message").textContent =
+        "GAME OVER 😪 Clickea en Otra vez para reiniciar!";
+      document.querySelector(".guess").disabled = true;
+      document.querySelector("body").style.backgroundColor = "#a30e0e";
+      document.querySelector(".number").textContent = secretNumber;
+    } else {
+      document.querySelector(".message").textContent = "Muy alto!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    }
   } else {
-    document.querySelector(".message").textContent = "Muy bajo!";
-    score--;
-    document.querySelector(".score").textContent = score;
+    if (score == 1) {
+      document.querySelector(".message").textContent =
+        "GAME OVER 😪 Clickea en Otra vez para reiniciar!";
+      document.querySelector(".guess").disabled = true;
+      document.querySelector("body").style.backgroundColor = "#a30e0e";
+      document.querySelector(".number").textContent = secretNumber;
+    } else {
+      document.querySelector(".message").textContent = "Muy bajo!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    }
   }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".guess").disabled = false;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".message").textContent = "Empeza a adivinar";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".guess").value = "";
 });
