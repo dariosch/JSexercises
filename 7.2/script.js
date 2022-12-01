41c040d8fe60e9dff152edbce154ccfe8183c67e
Dario <dario.sch@gmail.com>
2022-12-01 20:44:13 -0300

    added functions for modal opening and closing with buttons and keys

--- 7.2/script.js
+++ 7.2/script.js
@@ -2,5 +2,30 @@
 
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
-const btn = document.querySelector(".close-modal");
+const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+const openModal = function () {
+  //console.log("Button clicked");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", openModal);
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+//respond to keyboard events
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
