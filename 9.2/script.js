fb15601704ff82054ea075186903e55d8624abeb
Dario <dario.sch@gmail.com>
2023-01-23 18:20:36 -0300

    Coding challenge 2 completo

---
+++ 9.2/script.js
@@ -0,0 +1,85 @@
+"use strict";
+
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// Challenge: exercise 1
+// Loop over the game.scored array and print each player game to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+for (const [i, player] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${player}`);
+}
+
+// Challenge: exercise 2
+// Use a loop to calculate the average odd and log it to the console
+let promedio = 0;
+for (const odd of Object.values(game.odds)) {
+  promedio += odd;
+}
+promedio /= Object.values(game.odds).length;
+console.log(promedio);
+
+// Challenge: exercise 3
+/* Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+    Odd of victory Bayern Munich: 1.33
+    Odd of draw: 3.25
+    Odd of victory Borrussia Dortmund: 6.5
+
+    Get the team names directly from the game object, don't hardcode them (except for "draw"). Note how the odds and the game objects have the same property names.
+*/
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === "x" ? "draw" : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr}: ${odd}`);
+}
+
+// Challenge: exercise 4
+/*
+    Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+    {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+    }
+*/
+const scorers = {};
+for (const player of game.scored) {
+  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+}
+console.log(scorers);
